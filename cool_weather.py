6bf79b1ff09e15770a45292616a0e6cc1e7c9977
florije1988 <florije1988@gmail.com>
2015-01-28 13:08:28 +0800

    添加路由到配置文件中。

--- cool_weather.py
+++ cool_weather.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
 
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+# if __name__ == '__main__':
+#     app.run()
 
-app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+a = '%s'
+print a
--- weather/config/default.py
+++ weather/config/default.py
@@ -18,6 +18,13 @@ class Config:
     LOG_PATH = 'logs'
     LOG_NAME = 'proj_logs.log'
 
+    province_url = 'http://www.weather.com.cn/data/city3jdata/china.html'  # 获取省份
+    district_url = 'http://weather.com.cn/data/city3jdata/provshi/%s.html'  # 获取区域
+    city_url = 'http://weather.com.cn/data/city3jdata/station/%s%s.html'  # 获取城市
+
+    current_day = 'http://weather.com.cn/data/cityinfo/%s%s%s.html'  # 获取当天天气信息
+    current_time = 'http://weather.com.cn/data/sk/%s%s%s.html'  # 获取实时天气信息
+
     @staticmethod
     def init_app(app):
         pass
